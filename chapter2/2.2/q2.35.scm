cbb437bf35714a9c84a0b5c66df3b6b7b28215ac
shindeajinkya <ajitshinde635@gmail.com>
2026-04-01 21:01:15 +0530

    chore: exercise 2.35

---
+++ chapter2/2.2/q2.35.scm
@@ -0,0 +1,30 @@
+(define (accumulate op initial sequence)
+        (if 
+            (null? sequence)
+            initial
+            (op 
+                (car sequence)
+                (accumulate op initial (cdr sequence))
+            )
+        )
+)
+
+(define (count-leaves t)
+    (accumulate 
+        (lambda (x y) (+ x y)) 
+        0 
+        (map 
+            (lambda (x) (
+                if
+                    (pair? x)
+                    (count-leaves x)
+                    1
+            )) 
+            t
+        )
+    )
+)
+
+(define x (cons (list 1 2) (list 3 4)))
+
+(display (count-leaves (list x x)))
